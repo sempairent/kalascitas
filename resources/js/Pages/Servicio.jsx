59d5bf992f0f41d8c0ec52163c1f212ee1908a25
miwa <sempairent@gmail.com>
2024-01-22 10:28:28 -0500

    servicios

--- resources/js/Pages/Servicio.jsx
+++ resources/js/Pages/Servicio.jsx
@@ -6,7 +6,7 @@ export default function Servicio(props) {
         <AuthenticatedLayout
             auth={props.auth}
             errors={props.errors}
-            /*header={<h2 className="font-semibold text-xl text-gray-800 leading-tight">Dashboard</h2>}*/
+        /*header={<h2 className="font-semibold text-xl text-gray-800 leading-tight">Dashboard</h2>}*/
         >
             <Head title="Dashboard" />
 
@@ -14,8 +14,48 @@ export default function Servicio(props) {
                 <div className="max-w-7xl mx-auto sm:px-6 lg:px-8">
                     <div className="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                         <div className="p-6 text-gray-900">
-                            buenvenido a Servicio
+                            <h5 className='text-lg font-sans font-semibold'>Bienvenido a Servicios</h5>
                         </div>
+
+                        <div className='flex space-x-4'>
+                            <a href="#" class="flex flex-col items-center bg-white border border-gray-200 rounded-lg shadow md:flex-row md:max-w-xl hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700">
+                                <img class="object-cover w-full rounded-t-lg h-96 md:h-auto md:w-48 md:rounded-none md:rounded-s-lg" src="https://flowbite.com/docs/images/blog/image-4.jpg" alt="" />
+                                <div class="flex flex-col justify-between p-4 leading-normal">
+                                    <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Servicio de Emergencias y Urgencias</h5>
+                                    <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">las kalas</p>
+                                </div>
+                            </a>
+
+
+                            <a href="#" class="flex w-full items-center bg-white border border-gray-200 rounded-lg shadow md:flex-row md:max-w-xl hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700">
+                                <img class="object-cover w-full rounded-t-lg h-96 md:h-auto md:w-48 md:rounded-none md:rounded-s-lg" src="https://flowbite.com/docs/images/blog/image-4.jpg" alt="" />
+                                <div class="flex flex-col justify-between p-4 leading-normal">
+                                    <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Servicio de Hospitalización</h5>
+                                    <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">las kalas</p>
+                                </div>
+                            </a>
+                        </div>
+                        <br />
+                        <div className='flex space-x-4'>
+                            <a href="#" class="flex w-full items-center bg-white border border-gray-200 rounded-lg shadow md:flex-row md:max-w-xl hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700">
+                                <img class="object-cover w-full rounded-t-lg h-96 md:h-auto md:w-48 md:rounded-none md:rounded-s-lg" src="https://flowbite.com/docs/images/blog/image-4.jpg" alt="" />
+                                <div class="flex flex-col justify-between p-4 leading-normal">
+                                    <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Unidades de apoyo al Diagnostico</h5>
+                                    <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">las kalas</p>
+                                </div>
+                            </a>
+
+
+                            <a href="#" class="flex w-full items-center bg-white border border-gray-200 rounded-lg shadow md:flex-row md:max-w-xl hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700">
+                                <img class="object-cover w-full rounded-t-lg h-96 md:h-auto md:w-48 md:rounded-none md:rounded-s-lg" src="https://flowbite.com/docs/images/blog/image-4.jpg" alt="" />
+                                <div class="flex flex-col justify-between p-4 leading-normal">
+                                    <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Centro Quirúrgico  </h5>
+                                    <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">las kalas</p>
+                                </div>
+                            </a>
+                        </div>
+
+
                     </div>
                 </div>
             </div>
--- resources/js/Pages/Welcome.jsx
+++ resources/js/Pages/Welcome.jsx
@@ -27,7 +27,7 @@ export default function Welcome(props) {
                             </Link>
                         ) : (
                             <>
-                            
+
                                 <Link href={route('login')} className="text-sm text-black dark:text-black underline">
                                     <button className='h-12 bg-gradient-to-r from-cyan-500 to-blue-500 text-white font-semibold py-2 px-4 border border-gray-400 rounded shadow '>
                                         Iniciar Sesión
@@ -70,7 +70,7 @@ export default function Welcome(props) {
                                             <table className="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
                                                 <thead className="text-xs text-gray-700 uppercase dark:text-gray-400">
                                                     <tr>
-                                                        
+
                                                     </tr>
                                                 </thead>
                                                 <br />
@@ -80,37 +80,37 @@ export default function Welcome(props) {
                                                             Lunes
                                                         </th>
                                                         <td className="px-6 py-4">
-                                                        07:00 - 21:00
+                                                            07:00 - 21:00
                                                         </td>
 
 
                                                     </tr>
-                                                    <tr class="border-b border-gray-200 dark:border-gray-700">
+                                                    <tr className="border-b border-gray-200 dark:border-gray-700">
                                                         <th scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap  dark:text-white bg-black/30">
                                                             Martes
                                                         </th>
                                                         <td className="px-6 py-4">
-                                                        07:00 - 21:00
+                                                            07:00 - 21:00
                                                         </td>
 
 
                                                     </tr>
-                                                    <tr class="border-b border-gray-200 dark:border-gray-700">
+                                                    <tr className="border-b border-gray-200 dark:border-gray-700">
                                                         <th scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap  dark:text-white bg-black/30">
                                                             Miercoles
                                                         </th>
                                                         <td className="px-6 py-4">
-                                                        07:00 - 21:00
+                                                            07:00 - 21:00
                                                         </td>
 
 
                                                     </tr>
-                                                    <tr class="border-b border-gray-200 dark:border-gray-700">
+                                                    <tr className="border-b border-gray-200 dark:border-gray-700">
                                                         <th scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white bg-black/30">
                                                             Jueves
                                                         </th>
                                                         <td className="px-6 py-4">
-                                                        07:00 - 21:00   
+                                                            07:00 - 21:00
                                                         </td>
 
 
@@ -120,7 +120,7 @@ export default function Welcome(props) {
                                                             viernes
                                                         </th>
                                                         <td className="px-6 py-4">
-                                                        07:00 - 21:00
+                                                            07:00 - 21:00
                                                         </td>
 
 
@@ -130,7 +130,7 @@ export default function Welcome(props) {
                                                             Sabado
                                                         </th>
                                                         <td className="px-6 py-4">
-                                                        08:00 - 20:00
+                                                            08:00 - 20:00
                                                         </td>
 
 
@@ -140,7 +140,7 @@ export default function Welcome(props) {
                                                             Domingo
                                                         </th>
                                                         <td className="px-6 py-4">
-                                                        08:00 - 20:00
+                                                            08:00 - 20:00
                                                         </td>
 
 
